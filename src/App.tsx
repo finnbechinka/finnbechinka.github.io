b42587191710db1e10071e7b1a98d3b9cfb54ba2
Finn <contact@bechinka.eu>
2023-03-29 02:05:03 +0200

    feat: add my name :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <></>
+      <div className="banner">
+        <h1>Finn's GitHub Page</h1>
+        <p>:)</p>
+      </div>
     </div>
   );
 }
