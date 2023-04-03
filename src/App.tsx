80b1dbeaff1d1363d6ee1ef7068e201e3dfb6dc0
Finn <contact@bechinka.eu>
2023-04-03 19:48:17 +0200

    feat: add mouse hover dot thingy effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,33 @@ import "./App.css";
 import githubLogo from "./assets/github.svg";
 import linkedinLogo from "./assets/linkedin.svg";
 import stackoverflowLogo from "./assets/stackoverflow.svg";
+import MouseEffect from "./components/MouseEffect";
 
 function App() {
   return (
-    <div className="App">
-      <div className="banner">
-        <h1>Finn's GitHub Page</h1>
-        <p>:)</p>
+    <>
+      <div className="App">
+        <MouseEffect />
+        <div className="centerContainer">
+          <div className="banner">
+            <h1>Finn's GitHub Page</h1>
+            <p>:)</p>
+          </div>
+          <div className="socials">
+            <a href="https://www.linkedin.com/in/finn-b-178a2b18b" target="_blank">
+              <img src={linkedinLogo} className="logo" alt="LinkedIn logo" />
+            </a>
+            <a href="https://github.com/finnbechinka" target="_blank">
+              <img src={githubLogo} className="logo" alt="GitHub logo" />
+            </a>
+            <a href="https://stackoverflow.com/users/16952372" target="_blank">
+              <img src={stackoverflowLogo} className="logo" alt="Stack Overflow logo" />
+            </a>
+          </div>
+        </div>
+        <MouseEffect />
       </div>
-      <div className="socials">
-        <a href="https://www.linkedin.com/in/finn-b-178a2b18b" target="_blank">
-          <img src={linkedinLogo} className="logo" alt="LinkedIn logo" />
-        </a>
-        <a href="https://github.com/finnbechinka" target="_blank">
-          <img src={githubLogo} className="logo" alt="GitHub logo" />
-        </a>
-        <a href="https://stackoverflow.com/users/16952372" target="_blank">
-          <img src={stackoverflowLogo} className="logo" alt="Stack Overflow logo" />
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/MouseEffect.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "../styles/MouseEffect.css";
+
+function MouseEffect() {
+  function spawnSpans() {
+    let spans = [];
+    for (let i = 0; i < 300; i++) {
+      spans.push(<span />);
+    }
+    spans[0];
+    return spans;
+  }
+  return <div className="dotContainer">{spawnSpans()}</div>;
+}
+
+export default MouseEffect;
